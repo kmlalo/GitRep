6c932c035584ebde1e369907f41293bc766f01c5
김광기 <@pcsw-kkg>
2010-07-30 18:02:07 +0900

    new branch :: Hello World Git

--- src/HelloWorldGit.java
+++ src/HelloWorldGit.java
@@ -7,6 +7,8 @@ public class HelloWorldGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" Hello World Git");
+		
+		System.out.println(" Hello World Git");
 	}
 
 }
